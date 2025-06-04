7924312cec0448f3e10353cc777016128b805f69
ntnn19 <natannagar89@gmail.com>
2025-06-04 16:34:21 +0200

    added profile to run.sh#

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ weights_dir=$3
 database_dir=$4
 mmcif_dir=$5
 mkdir -p $output_dir
-snakemake -s luca.smk --configfile config/config.yaml --use-singularity --singularity-args "--nv -B $(realpath $database_dir):/database -B $(realpath $input_dir):/input -B $(realpath $output_dir):/output/predictions -B  -B $(realpath $weights_dir):/cache -B $(realpath $mmcif_dir):/mmcif_dir" -n
+snakemake --workflow-profile profile -s luca.smk --configfile config/config.yaml --use-singularity --singularity-args "--nv -B $(realpath $database_dir):/database -B $(realpath $input_dir):/input -B $(realpath $output_dir):/output/predictions -B  -B $(realpath $weights_dir):/cache -B $(realpath $mmcif_dir):/mmcif_dir" -n
