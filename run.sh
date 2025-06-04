7393bdb686e81f0752cf5e1fc4e3eef8074b01c2
ntnn19 <natannagar89@gmail.com>
2025-06-04 23:01:20 +0200

    modified run.sh to submit 10 jobs with -j 10

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ weights_dir=$3
 database_dir=$4
 mmcif_dir=$5
 mkdir -p $output_dir/predictions
-snakemake -p --workflow-profile profile -s luca.smk --configfile config/config.yaml --use-singularity --singularity-args "--nv -B $(realpath $database_dir):/database -B $(realpath $input_dir):/input -B $(realpath $output_dir)/predictions:/predictions  -B $(realpath $weights_dir):/cache -B $(realpath $mmcif_dir):/mmcif_dir" -j 1
+snakemake -p --workflow-profile profile -s luca.smk --configfile config/config.yaml --use-singularity --singularity-args "--nv -B $(realpath $database_dir):/database -B $(realpath $input_dir):/input -B $(realpath $output_dir)/predictions:/predictions  -B $(realpath $weights_dir):/cache -B $(realpath $mmcif_dir):/mmcif_dir" -j 10
